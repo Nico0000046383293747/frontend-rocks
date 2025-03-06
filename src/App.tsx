7b2896e6d8511244ac8c1ef6744967c6f2cf1a17
Nico0000046383293747 <nicolofolicaldi@gmail.com>
2025-03-06 11:26:53 +0000

    SECOND

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ export const App = () => {
 
           <button
            className="bg-red-700 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-red-600 transition-colors"
-          onClick = {() => setTitle("Charizard")}
+          onClick = {() => setTitle("CharizarD")}
           >
           CHANGE THE TITOL 
          </button>
